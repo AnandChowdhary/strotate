13a39ba373891b3130a57c2578d0cd6d022c7b1e
Anand Chowdhary <mail@anandchowdhary.com>
2019-04-18 20:54:11 +0200

    :white_check_mark: Add tests

--- tests/random.test.ts
+++ tests/random.test.ts
@@ -1,5 +1,13 @@
-import { random } from "../src/random";
+import { rot13 } from "../";
 
 test("should be a string", () => {
-  expect(typeof random()).toBe("string");
+  expect(typeof rot13("Hello, world!")).toBe("string");
+});
+
+test("should return correct rot 13", () => {
+  expect(rot13("Hello, world!")).toBe("Uryyb, jbeyq!");
+});
+
+test("should return correct un-rot 13", () => {
+  expect(rot13("Uryyb, jbeyq!")).toBe("Hello, world!");
 });
